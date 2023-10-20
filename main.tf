d76457a9c4c617933cf1dcad8955f09b9eb49098
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-20 18:35:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals{ 
  storage_name= ["north","south","east","west"]
- clusters_name= ["mcit1","mcit2","mcit3","mcit4"]
+ clusters_name= ["mcit"]
 
  }
   resource "azurerm_resource_group" "butterfly" {
