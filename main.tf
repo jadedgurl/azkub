4b837a5378883d73008531ea7ad85fc1d9d85e42
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-10-20 18:29:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals{ 
  storage_name= ["north","south","east","west"]
- clusters_name= ["montrealcollege"]
+ clusters_name= ["MontrealCollegeCluster1","MontrealCollegeCluster2","MontrealCollegeCluster3","MontrealCollegeCluster4"]
 
  }
   resource "azurerm_resource_group" "butterfly" {
